19a6f47660f44d0793ea5abe27562391bcb0209f
Susan Buck <susan@susanbuck.net>
2013-09-30 06:59:11 -0400

    Removed debug.

--- debug.php
+++
@@ -1,6 +0,0 @@
-<th>Logs Directory</th>
-<?php if (is_dir(APPPATH) AND is_dir(APPPATH.'logs') AND is_writable(APPPATH.'logs')): ?>
-	<td class="pass"><?php echo APPPATH.'logs/' ?></td>
-<?php else: $failed = TRUE ?>
-	<td class="fail">The <code><?php echo APPPATH.'logs/' ?></code> directory is not writable.</td>
-<?php endif ?>
