e5ff06008960b7b95db4ec2e30e61af20ffcfcee
Susan Buck <susan@susanbuck.net>
2013-09-30 04:33:02 -0400

    Triggering change

--- samples/sample-app.com/views/v_index_index.php
+++ samples/sample-app.com/views/v_index_index.php
@@ -1,8 +1,7 @@
 <pre>
-Hello World! You have succesfully spawned a new application.
-
-This message is being triggered via the c_index.php controller, within the index() method.
-
-The view is v_index_index.php.
-
+	Hello World! You have succesfully spawned a new application.
+	
+	This message is being triggered via the c_index.php controller, within the index() method.
+	
+	The view is v_index_index.php.
 </pre>
