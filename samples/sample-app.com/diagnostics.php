a74f480fff3fcb811f3709a1ccc20525a95b347d
Susan Buck <susan@susanbuck.net>
2013-10-02 04:46:53 -0400

    Added diagnostics page.

---
+++ samples/sample-app.com/diagnostics.php
@@ -0,0 +1,65 @@
+<style>
+	body {
+		font-family:Consolas;
+	}
+
+	.pass {
+		color:green;
+	}
+	
+	.fail {
+		color:red;
+	}
+</style>
+
+<?php
+$apache_modules = apache_get_modules();
+$mod_rewrite    = in_array('mod_rewrite',$apache_modules);
+
+$ini = ini_get_all();
+
+$app_path = realpath(dirname(__FILE__)).'/';;
+$doc_root = $_SERVER['DOCUMENT_ROOT'].'/';
+
+$environment = file_exists($doc_root."../environment.php");
+
+$core = file_exists($doc_root."../core/");
+?>
+
+
+<?php if(isset($_GET['phpinfo'])): ?>
+	<a href='diagnostics.php'>&larr; Go back</a>
+	<?php phpinfo(); ?>
+<?php endif; ?>
+
+APP Path: <?php echo $app_path  ?>
+<br>
+Doc Root: <?php echo $doc_root; ?>
+<br>
+PHP Version: <?php echo phpversion(); ?>
+<br>
+Server timezone: <?php echo date_default_timezone_get(); ?>
+<br>
+Short open tags: <?php print_r($ini['short_open_tag']); ?>
+
+<?php if($environment): ?>
+	<div class='pass'>environment.php exists</div>
+<?php else: ?>
+	<div class='fail'>environment.php is missing</div>
+<?php endif; ?>
+
+<?php if($core): ?>
+	<div class='pass'>core/ exists</div>
+<?php else: ?>
+	<div class='fail'>core/ is missing</div>
+<?php endif; ?>
+
+<?php if($mod_rewrite): ?>
+	<div class='pass'>mod_rewrite is enabled</div>
+<?php else: ?>
+	<div class='fail'>mod_rewrite is not enabled</div>
+<?php endif; ?>
+
+<br><a href='?phpinfo=true'>Run phpinfo()</a>
+
+
--- samples/sample-app.com/views/v_index_index.php
+++ samples/sample-app.com/views/v_index_index.php
@@ -1,8 +1,11 @@
-<pre>
-Hello World! You have succesfully spawned a new application.
+<p>
+	Hello World! You have successfully spawned a new application.
+</p>
 
-This message is being triggered via the c_index.php controller, within the index() method.
+<p>
+	This message is being triggered via the c_index.php controller, within the index() method.
+</p>
 
-The view is v_index_index.php.
-
-</pre>
+<p>
+	<strong>Since everything is in working order, you should now delete <?php echo APP_PATH?>diagnostics.php</strong>
+</p>
