71ae965b1f5d32347f56a3cb7eb21a984ee9cb20
Susan Buck <susan@susanbuck.net>
2013-10-09 02:17:49 -0400

    apache_get_modules() often doesn't work on shared servers; remove this test.

--- samples/sample-app.com/config/config.php
+++ samples/sample-app.com/config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "myapp_com"); 
+	define('DB_NAME', 'myapp_com'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
--- samples/sample-app.com/diagnostics.php
+++ samples/sample-app.com/diagnostics.php
@@ -12,10 +12,14 @@
 	}
 </style>
 
-<?php
-$apache_modules = apache_get_modules();
-$mod_rewrite    = in_array('mod_rewrite',$apache_modules);
+<a href='?phpinfo=true'>Run phpinfo()</a>
+<?php if(isset($_GET['phpinfo'])): ?>
+	<a href='diagnostics.php'>&larr; Go back</a>
+	<?php die(phpinfo()); ?>
+<?php endif; ?>
+<br><br>
 
+<?php
 $ini = ini_get_all();
 
 $app_path = realpath(dirname(__FILE__)).'/';;
@@ -25,12 +29,6 @@ $environment = file_exists($doc_root."../environment.php");
 
 $core = file_exists($doc_root."../core/");
 ?>
-
-
-<?php if(isset($_GET['phpinfo'])): ?>
-	<a href='diagnostics.php'>&larr; Go back</a>
-	<?php phpinfo(); ?>
-<?php endif; ?>
 
 APP Path: <?php echo $app_path  ?>
 <br>
@@ -51,12 +49,5 @@ PHP Version: <?php echo phpversion(); ?>
 	<div class='fail'>core/ is missing</div>
 <?php endif; ?>
 
-<?php if($mod_rewrite): ?>
-	<div class='pass'>mod_rewrite is enabled</div>
-<?php else: ?>
-	<div class='fail'>mod_rewrite is not enabled</div>
-<?php endif; ?>
-
-<br><a href='?phpinfo=true'>Run phpinfo()</a>
 
 
